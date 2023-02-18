38e87949120df632990c3b7a9f1e7a3437bf86eb
AlexMerk83 <a.merkushenkov@gmail.com>
2023-02-18 22:45:59 +0200

    Add Task38

--- Program.cs
+++ Program.cs
@@ -24,14 +24,14 @@ void main()
     }
 }
 
-
+#region Task34 EvenNumQuantity
 // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 void Task34EvenNumQuantity()
 {
     Console.Clear();
-    System.Console.WriteLine("A new array will be generated filled with random 3-digit integer numbers.");
+    System.Console.WriteLine("A new array filled with random 3-digit integer numbers shall be generated.");
 
     int[] array = ArrayHandler.GetRandomIntArray(ConsoleIOHandler.ReadInt("the array length", 1, 1000), 100, 999);
     
@@ -43,9 +43,11 @@ void Task34EvenNumQuantity()
         if (array[i] % 2 == 0)
             evenNumCount++;
 
-    System.Console.WriteLine($"Quantity of even numbers in the array is {evenNumCount}");
+    System.Console.WriteLine($"The quantity of even numbers in the array is {evenNumCount}");
 }
+#endregion
 
+#region Task36 SumOfOddElements
 // Задача 36: Задайте одномерный массив, заполненный случайными числами.
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
@@ -53,9 +55,9 @@ void Task34EvenNumQuantity()
 void Task36SumOfOddElements()
 {
     Console.Clear();
-    System.Console.WriteLine("Enter parameters of a random array.");
+    System.Console.WriteLine("Enter parameters of an array filled with random integer numbers.");
 
-    int[] array = ArrayHandler.GetRandomIntArray(ConsoleIOHandler.ReadInt("the array length"),
+    int[] array = ArrayHandler.GetRandomIntArray(ConsoleIOHandler.ReadInt("the array length", 1, 1000),
                                                     ConsoleIOHandler.ReadInt("the minimum value"),
                                                             ConsoleIOHandler.ReadInt("the maximum value"));
     
@@ -66,16 +68,29 @@ void Task36SumOfOddElements()
     for (int i = 1; i < array.Length; i+=2)
         evenElementsSum += array[i];
 
-    System.Console.WriteLine($"Sum of the numbers in the odd positions is {evenElementsSum}");
+    System.Console.WriteLine($"The sum of the numbers in odd positions in the array is {evenElementsSum}");
 }
+#endregion
 
+#region Task38 MaxMinNumDifference
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 void Task38MaxMinNumDifference()
 {
-    
-}
+    Console.Clear();
+    System.Console.WriteLine("Enter the parameters of an array filled with random real numbers.");
 
+    double[] array = ArrayHandler.GetRandomDoubleArray(ConsoleIOHandler.ReadInt("the array length", 1, 1000),
+                                                    ConsoleIOHandler.ReadDouble("the minimum value"),
+                                                            ConsoleIOHandler.ReadDouble("the maximum value"));
+    
+    System.Console.WriteLine($"[{string.Join(", ", array)}]");
+
+    double[] minMax = ArrayHandler.GetMinAndMaxElements(array);
+    
+    System.Console.WriteLine($"The difference between the maximum and minimum numbers in the array is {minMax[1]-minMax[0]}");    
+}
+#endregion
 
 
 #region Input and Output methods
@@ -123,42 +138,43 @@ class ConsoleIOHandler
 
     #region Input Support Methods
 
-    public const string defaultErrorMessage = "Input error. Please enter an integer number.";
+    public const string defaultIntErrorMessage = "Input error. Please enter an integer number.";
     private const string defaultRangeErrorMessage = "Input error. Please enter an integer number from the range {0}..{1}.";
-
+    public const string defaultDoubleErrorMessage = "Input error. Please enter a real number.";
+    
     public static int ReadInt(string argument)
     {
-        int intNum = 0,
+        int parsedNum = 0,
             inputFieldX = 0,
             inputFieldY = 0;
 
         System.Console.Write($"Enter {argument}: ");
         inputFieldX = Console.CursorLeft;
         inputFieldY = Console.CursorTop;
-        while (!int.TryParse(Console.ReadLine(), out intNum))
+        while (!int.TryParse(Console.ReadLine(), out parsedNum))
         {
             Console.SetCursorPosition(0, inputFieldY);
             ClearLine();
             System.Console.WriteLine($"Enter {argument}: ");
-            System.Console.WriteLine(defaultErrorMessage);
+            System.Console.WriteLine(defaultIntErrorMessage);
             Console.SetCursorPosition(inputFieldX, inputFieldY);
         }
 
         ClearLine();
         
-        return intNum;
+        return parsedNum;
     }
 
     public static int ReadInt(string argument, int minValue, int maxValue, string errorMessage = defaultRangeErrorMessage)
     {
-        int intNum = 0,
+        int parsedNum = 0,
             inputFieldX = 0,
             inputFieldY = 0;
 
         System.Console.Write($"Enter {argument}: ");
         inputFieldX = Console.CursorLeft;
         inputFieldY = Console.CursorTop;
-        while (!int.TryParse(Console.ReadLine(), out intNum) || intNum < minValue || intNum > maxValue)
+        while (!int.TryParse(Console.ReadLine(), out parsedNum) || parsedNum < minValue || parsedNum > maxValue)
         {
             Console.SetCursorPosition(0, inputFieldY);
             ClearLine();
@@ -169,13 +185,33 @@ class ConsoleIOHandler
 
         ClearLine();
         
-        return intNum;
+        return parsedNum;
     }
 
+    public static double ReadDouble(string argument)
+    {
+        double parsedNum = 0.0;
+        int inputFieldX = 0,
+            inputFieldY = 0;
+
+        System.Console.Write($"Enter {argument}: ");
+        inputFieldX = Console.CursorLeft;
+        inputFieldY = Console.CursorTop;
+        while (!double.TryParse(Console.ReadLine(), out parsedNum))
+        {
+            Console.SetCursorPosition(0, inputFieldY);
+            ClearLine();
+            System.Console.WriteLine($"Enter {argument}: ");
+            System.Console.WriteLine(defaultDoubleErrorMessage);
+            Console.SetCursorPosition(inputFieldX, inputFieldY);
+        }
+
+        ClearLine();
+        
+        return parsedNum;
+    }
     #endregion
-
 }
-
 #endregion
 
 
@@ -206,6 +242,21 @@ class ArrayHandler
 
         return arr;
     }
+
+    public static double[] GetMinAndMaxElements(double[] array)
+    {
+        double[] arrMinMax = new double[2];
+        
+        arrMinMax[0] = array[0];
+        arrMinMax[1] = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+            if (array[i] < arrMinMax[0]) arrMinMax[0] = array[i];
+            else if (array[i] > arrMinMax[1]) arrMinMax[1] = array[i];
+
+        return arrMinMax;
+    }
+
 }
 
 #endregion
