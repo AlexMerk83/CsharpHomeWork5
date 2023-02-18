6eef021f8f65daab0756345f803eefa46524b259
AlexMerk83 <a.merkushenkov@gmail.com>
2023-02-18 18:47:14 +0200

    Add tasks 34 and 36

--- Program.cs
+++ Program.cs
@@ -1,99 +1,4 @@
-﻿#region Output Support Methods
-
-string msgAnyKeyRequest = "Press any key to continue...";
-string msgNoSuchOption = "There is no such option. Try again.";
-
-
-void PrintMainMenu()
-{
-    Console.Clear();
-
-    System.Console.WriteLine();
-    System.Console.WriteLine("Homework 5 tasks:");
-    System.Console.WriteLine("1 - Task 34: Quantity of even numbers in an array");
-    System.Console.WriteLine("2 - Task 36: Sum of odd elements of an array");
-    System.Console.WriteLine("3 - Task 38: Difference between max and min numbers in an array");
-    System.Console.WriteLine("0 - Exit");
-    System.Console.Write("Enter a task nunber: ");    
-}
-
-void WaitForAnyKey()
-{
-        System.Console.WriteLine();
-        System.Console.WriteLine(msgAnyKeyRequest);
-        Console.ReadKey();
-}
-
-void ClearLine(int lineShift = 0, bool keepCursor = true)
-{
-    int currentTop = Console.CursorTop,
-        currentLeft = Console.CursorLeft;
-    
-    Console.SetCursorPosition(0, currentTop + lineShift);
-    Console.Write(new string(' ', Console.WindowWidth));
-
-    if (keepCursor)
-        Console.SetCursorPosition(currentLeft, currentTop);
-    else
-        Console.SetCursorPosition(0, currentTop + lineShift);
-}
-
-#endregion
-
-#region Input Support Methods
-
-int ReadInt(string argument)
-{
-    int intNum = 0,
-        inputFieldX = 0,
-        inputFieldY = 0;
-
-    System.Console.Write($"Enter {argument}: ");
-    inputFieldX = Console.CursorLeft;
-    inputFieldY = Console.CursorTop;
-    while (!int.TryParse(Console.ReadLine(), out intNum))
-    {
-        Console.SetCursorPosition(0, inputFieldY);
-        ClearLine();
-        System.Console.WriteLine($"Enter {argument}: ");
-        System.Console.WriteLine("Input error. This is not an integer number. Try again...");
-        Console.SetCursorPosition(inputFieldX, inputFieldY);
-    }
-
-    ClearLine();
-    
-    return intNum;
-}
-
-#endregion
-
-#region Array Methods
-
-int[] GetRandomIntArray(int length, int minVal, int maxVal)
-{
-    int[] arr = new int[length];
-
-    Random rnd = new Random();
-
-    for (int i = 0; i < length; i++)
-        arr[i] = rnd.Next(minVal, maxVal + 1);
-    
-    return arr;
-}
-
-double[] GetRandomDoubleArray(int length, double minVal, double maxVal)
-{
-    double[] arr = new double[length];
-
-    Random rnd = new Random();
-
-    for (int i = 0; i < length; i++)
-        arr[i] = minVal + rnd.NextDouble() * (maxVal - minVal);
-
-    return arr;
-}
-
-#endregion
+﻿main();
 
 void main()
 {
@@ -101,56 +6,206 @@ void main()
 
     while (isWork)
     {
-        PrintMainMenu();
+        ConsoleIOHandler.PrintMainMenu();
 
-        if (int.TryParse(Console.ReadLine(), out int taskNo))
+        int taskNo = ConsoleIOHandler.ReadInt("a task number", 0, 3, ConsoleIOHandler.msgNoSuchOption);
+
+        switch (taskNo)
         {
-            System.Console.WriteLine();
-
-            switch (taskNo)
-            {
-                case 1: Task34EvenNumQuantity(); break;
-                case 2: Task36SumOfOddElements(); break;
-                case 3: Task38MaxMinNumDiff(); break;
-                case 0: isWork = false; break;
-                default: System.Console.WriteLine(msgNoSuchOption); break;
-            }
-            
-            if (isWork)
-                WaitForAnyKey();
-            
+            case 1: Task34EvenNumQuantity(); break;
+            case 2: Task36SumOfOddElements(); break;
+            case 3: Task38MaxMinNumDifference(); break;
+            case 0: isWork = false; break;
+            default: System.Console.WriteLine(ConsoleIOHandler.msgNoSuchOption); break;
         }
-        else
-        {
-            System.Console.WriteLine();
-            System.Console.WriteLine(msgNoSuchOption);
 
-            WaitForAnyKey();
-        }
+        if (isWork)
+            ConsoleIOHandler.WaitForAnyKey();
     }
 }
 
 
-// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 void Task34EvenNumQuantity()
 {
+    Console.Clear();
+    System.Console.WriteLine("A new array will be generated filled with random 3-digit integer numbers.");
 
+    int[] array = ArrayHandler.GetRandomIntArray(ConsoleIOHandler.ReadInt("the array length", 1, 1000), 100, 999);
+    
+    System.Console.WriteLine($"[{string.Join(", ", array)}]");
+
+    int evenNumCount = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0)
+            evenNumCount++;
+
+    System.Console.WriteLine($"Quantity of even numbers in the array is {evenNumCount}");
 }
 
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 void Task36SumOfOddElements()
 {
+    Console.Clear();
+    System.Console.WriteLine("Enter parameters of a random array.");
+
+    int[] array = ArrayHandler.GetRandomIntArray(ConsoleIOHandler.ReadInt("the array length"),
+                                                    ConsoleIOHandler.ReadInt("the minimum value"),
+                                                            ConsoleIOHandler.ReadInt("the maximum value"));
     
+    System.Console.WriteLine($"[{string.Join(", ", array)}]");
+
+    int evenElementsSum = 0;
+
+    for (int i = 1; i < array.Length; i+=2)
+        evenElementsSum += array[i];
+
+    System.Console.WriteLine($"Sum of the numbers in the odd positions is {evenElementsSum}");
 }
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
-void Task38MaxMinNumDiff()
+void Task38MaxMinNumDifference()
 {
     
 }
 
-main();
+
+
+#region Input and Output methods
+class ConsoleIOHandler
+{
+    #region Output Support Methods
+
+    public const string msgAnyKeyRequest = "Press any key to continue...";
+    public const string msgNoSuchOption = "There is no such option. Try again.";
+
+
+    public static void PrintMainMenu()
+    {
+        Console.Clear();
+
+        System.Console.WriteLine("Homework 5 tasks:");
+        System.Console.WriteLine("1 - Task 34: Quantity of even numbers in an array");
+        System.Console.WriteLine("2 - Task 36: Sum of odd elements of an array");
+        System.Console.WriteLine("3 - Task 38: Difference between max and min numbers in an array");
+        System.Console.WriteLine("0 - Exit");
+    }
+
+    public static void WaitForAnyKey()
+    {
+            System.Console.WriteLine();
+            System.Console.WriteLine(msgAnyKeyRequest);
+            Console.ReadKey();
+    }
+
+    public static void ClearLine(int lineShift = 0, bool keepCursor = true)
+    {
+        int currentTop = Console.CursorTop,
+            currentLeft = Console.CursorLeft;
+        
+        Console.SetCursorPosition(0, currentTop + lineShift);
+        Console.Write(new string(' ', Console.WindowWidth));
+
+        if (keepCursor)
+            Console.SetCursorPosition(currentLeft, currentTop);
+        else
+            Console.SetCursorPosition(0, currentTop + lineShift);
+    }
+
+    #endregion
+
+    #region Input Support Methods
+
+    public const string defaultErrorMessage = "Input error. Please enter an integer number.";
+    private const string defaultRangeErrorMessage = "Input error. Please enter an integer number from the range {0}..{1}.";
+
+    public static int ReadInt(string argument)
+    {
+        int intNum = 0,
+            inputFieldX = 0,
+            inputFieldY = 0;
+
+        System.Console.Write($"Enter {argument}: ");
+        inputFieldX = Console.CursorLeft;
+        inputFieldY = Console.CursorTop;
+        while (!int.TryParse(Console.ReadLine(), out intNum))
+        {
+            Console.SetCursorPosition(0, inputFieldY);
+            ClearLine();
+            System.Console.WriteLine($"Enter {argument}: ");
+            System.Console.WriteLine(defaultErrorMessage);
+            Console.SetCursorPosition(inputFieldX, inputFieldY);
+        }
+
+        ClearLine();
+        
+        return intNum;
+    }
+
+    public static int ReadInt(string argument, int minValue, int maxValue, string errorMessage = defaultRangeErrorMessage)
+    {
+        int intNum = 0,
+            inputFieldX = 0,
+            inputFieldY = 0;
+
+        System.Console.Write($"Enter {argument}: ");
+        inputFieldX = Console.CursorLeft;
+        inputFieldY = Console.CursorTop;
+        while (!int.TryParse(Console.ReadLine(), out intNum) || intNum < minValue || intNum > maxValue)
+        {
+            Console.SetCursorPosition(0, inputFieldY);
+            ClearLine();
+            System.Console.WriteLine($"Enter {argument}: ");
+            System.Console.WriteLine(errorMessage, minValue, maxValue);
+            Console.SetCursorPosition(inputFieldX, inputFieldY);
+        }
+
+        ClearLine();
+        
+        return intNum;
+    }
+
+    #endregion
+
+}
+
+#endregion
+
+
+#region Array Methods
+
+class ArrayHandler
+{
+    public static int[] GetRandomIntArray(int length, int minVal, int maxVal)
+    {
+        int[] arr = new int[length];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < length; i++)
+            arr[i] = rnd.Next(minVal, maxVal + 1);
+        
+        return arr;
+    }
+
+    public static double[] GetRandomDoubleArray(int length, double minVal, double maxVal)
+    {
+        double[] arr = new double[length];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < length; i++)
+            arr[i] = minVal + rnd.NextDouble() * (maxVal - minVal);
+
+        return arr;
+    }
+}
+
+#endregion
